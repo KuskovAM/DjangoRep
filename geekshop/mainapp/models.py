7003f54f4522e1d069fdfe1ca1e101776c597ef7
KuskovAM <ravenkus2@gmail.com>
2019-04-29 00:34:14 +0300

    Работа с бд и админка

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    name = models.CharField(verbose_name='категория товара', max_length=255, unique=True)
+    description = models.TextField(verbose_name='Описание товара', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
